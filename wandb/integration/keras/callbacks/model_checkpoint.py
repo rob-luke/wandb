e899129772ddc2c47fbbafeca989e38548b03ee0
Rob Luke <code@robertluke.net>
2024-06-23 10:39:24 +1000

    fix: WandbModelCheckpoint options bug

--- wandb/integration/keras/callbacks/model_checkpoint.py
+++ wandb/integration/keras/callbacks/model_checkpoint.py
@@ -99,7 +99,7 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
             save_weights_only=save_weights_only,
             mode=mode,
             save_freq=save_freq,
-            options=options,
+            # options=options,  # https://github.com/wandb/wandb/issues/7710 rluke
             initial_value_threshold=initial_value_threshold,
             **kwargs,
         )
